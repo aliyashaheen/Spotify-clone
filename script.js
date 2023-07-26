bfaf1496d3762ea3f5b07aa37db01cd3f070c1eb
Aliya <shahenaliya756@gmail.com>
2023-07-26 22:47:40 +0530

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap');
+</style>
+body{
+    backgroung-color: antiquewhite ;
+}
+*{
+    margin: 0;
+    padding: 0;
+}
+
+nav{
+    font-family: 'Ubuntu', sans serif;
+}
